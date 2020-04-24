e072fa3c9eb7e36b661f552f5adf0eb2dd63ad53
eltonplima <eltonplima@gmail.com>
2020-04-24 09:36:00 +0100

    Add skeleton to update border radius
    Related issue: #2

--- implementations/1-beginner/border-radius/js/border_radius.js
+++ implementations/1-beginner/border-radius/js/border_radius.js
@@ -12,7 +12,28 @@ function update_target_value(target) {
     target.value = filter_valid_values(target.value)
 }
 
+function update_respective_border_radius(target) {
+    switch (target.id) {
+        case 'top-left':
+            console.log('update top-left')
+            break
+        case 'top-right':
+            console.log('update top-right')
+            break
+        case 'bottom-left':
+            console.log('update bottom-left')
+            break
+        case 'bottom-right':
+            console.log('update bottom-right')
+            break
+        default:
+            console.error(`The element ${target.id} cannot be used to update border-radius properties!`)
+    }
+    console.log(target.id)
+}
+
 function update_border_radius(event) {
     const target = event.target
     update_target_value(target)
+    update_respective_border_radius(target)
 }
