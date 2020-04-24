f8a0fcab6e1e7f55a83678fa37ea2823624c5663
eltonplima <eltonplima@gmail.com>
2020-04-24 09:18:44 +0100

    Refactor border_radius
    Created more specialized functions to improve readability of the
    function update_border_radius
    
    Related issue: #2

--- implementations/1-beginner/border-radius/js/border_radius.js
+++ implementations/1-beginner/border-radius/js/border_radius.js
@@ -4,7 +4,15 @@ for (input of inputs) {
     input.addEventListener('input', update_border_radius)
 }
 
+function filter_valid_values(value) {
+    return value.replace(/[^0-9]/, '')
+}
+
+function update_target_value(target) {
+    target.value = filter_valid_values(target.value)
+}
+
 function update_border_radius(event) {
     const target = event.target
-    target.value = target.value.replace(/[^0-9]/, '')
+    update_target_value(target)
 }
